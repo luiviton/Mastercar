cdfe401c9ee8178fc090af91eb3d5459c0d84d75
Roma <zdir@bk.ru>
2020-10-07 11:51:05 +0200

    add js

---
+++ js/javascript.js
@@ -0,0 +1,20 @@
+'use strict'
+
+$(function () {
+    $(window).scroll(function() {
+        var height = $(window).scrollTop();
+        if (height > 500) {
+            $('.back2Top').fadeIn();
+        } else {
+            $('.back2Top').fadeOut();
+        }
+    });
+    $(document).ready(function() {
+        $(".back2Top").click(function(event) {
+            event.preventDefault();
+            $("html, body").animate({ scrollTop: 0 }, "slow");
+            return false;
+        });
+
+    });
+});
